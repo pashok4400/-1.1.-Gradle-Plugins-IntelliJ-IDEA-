bff1fcdf7a0879f6d60494a52377802e851adf6d
Павел Сотников <pax4400@mail.ru>
2022-03-14 13:02:54 +0300

    build.grable

--- src/main/kotlin/ru/netology/kotlin/app.kt
+++ src/main/kotlin/ru/netology/kotlin/app.kt
@@ -1,3 +1,14 @@
 fun main() {
-    println("First App!")
+   //println("First App!")
+    val itemPrice = 100
+    val itemCount = 11
+    val discount = 10
+    val discountStart = 1_000
+
+    var totalPrice: Int = itemPrice * itemCount
+    if (totalPrice >= discountStart) {
+        totalPrice -= discount
+    }
+    println("total price: $totalPrice")
+
 }
